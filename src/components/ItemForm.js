803499ec7506f151c084b43cc4b7a4747bb65f31
Jocelyn Naarden <tzionone@gmail.com>
2022-06-03 17:23:59 -0400

    Code along: Add item to list

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,35 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({ updateItems }) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  //MUST PUT IN HANDLE SUBMIT FUNCTION*****
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(name, category)
+    const formData = {
+      name: name,
+      category: category,
+      isInCart: false
+    }
+    const serverURL = 'http://localhost:4000/items'
+    fetch(serverURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(r => r.json())
+      .then(data => {
+        updateItems(data)
+      })
+
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
         <input
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
+// import { useEffect } from "react/cjs/react.production.min";
 
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+  const serverURL = 'http://localhost:4000/items'
+  useEffect(() => {
+    fetch(serverURL)
+      .then(r => r.json())
+      .then(items => setItems(items))
+  }, [])
+
+  function addItemToList(newItem) {
+    setItems([...items, newItem])
+  }
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -19,7 +31,7 @@ function ShoppingList() {
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
+      <ItemForm updateItems={addItemToList} items={items} />
       <Filter
         category={selectedCategory}
         onCategoryChange={handleCategoryChange}
